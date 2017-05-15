5e9c934a1a256da36b3ec429c1fed25246f51ab9
pawel_tarkowski_clurgo <pawel.tarkowski@clurgo.com>
2017-05-16 01:52:24 +0200

    #4 fix search by controller

--- localization-finder-main/src/main/java/pl/tarko/software/finder/localization/controller/LocalizationSearchController.java
+++ localization-finder-main/src/main/java/pl/tarko/software/finder/localization/controller/LocalizationSearchController.java
@@ -3,6 +3,7 @@ package pl.tarko.software.finder.localization.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.tarko.software.finder.localization.model.dto.LocalizationDto;
@@ -28,7 +29,7 @@ public class LocalizationSearchController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String search(@Valid LocalizationSearchParamDto searchForm, Model model) {
+    public String search(@ModelAttribute(name ="searchForm") LocalizationSearchParamDto searchForm, Model model) {
         final List<LocalizationDto> results = localizationService.findLocalizations(searchForm);
         model.addAttribute("results", results);
         return SEARCH_VIEW_NAME;
