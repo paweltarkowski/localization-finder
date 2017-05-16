476a2d8ad16dbef8ce6eb5ebad3874708d3683c1
pawel_tarkowski_clurgo <pawel.tarkowski@clurgo.com>
2017-05-16 21:51:48 +0200

    #6 add liquibase base setup

--- localization-finder-core/src/main/java/pl/tarko/software/finder/LocalizationCoreConfiguration.java
+++ localization-finder-core/src/main/java/pl/tarko/software/finder/LocalizationCoreConfiguration.java
@@ -1,9 +1,29 @@
 package pl.tarko.software.finder;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 public class LocalizationCoreConfiguration {
+
+    @Value("${liquibase.change-log}")
+    private String changeLogDir;
+
+    @Value("${liquibase.contexts}")
+    private String liquibaseContexts;
+
+    @Bean(name = "liquibase")
+    public SpringLiquibase configLiquibase(DataSource configDataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(configDataSource);
+        liquibase.setChangeLog(changeLogDir);
+        liquibase.setContexts(liquibaseContexts);
+        return liquibase;
+    }
 }
--- localization-finder-core/src/test/java/pl/tarko/software/finder/CorePreConfiguredContextTest.java
+++ localization-finder-core/src/test/java/pl/tarko/software/finder/CorePreConfiguredContextTest.java
@@ -1,6 +1,10 @@
 package pl.tarko.software.finder;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,5 +12,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 @ContextConfiguration(classes = {LocalizationCoreConfiguration.class})
+@SpringBootTest
 public abstract class CorePreConfiguredContextTest {
 }
