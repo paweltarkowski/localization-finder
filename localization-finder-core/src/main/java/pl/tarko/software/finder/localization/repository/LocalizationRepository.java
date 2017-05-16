bc440a09da1cf822f79bb97670e9b0d317c523fb
pawel_tarkowski_clurgo <pawel.tarkowski@clurgo.com>
2017-05-17 01:02:55 +0200

    #12 add query dsl

--- localization-finder-core/src/main/java/pl/tarko/software/finder/localization/repository/LocalizationRepository.java
+++ localization-finder-core/src/main/java/pl/tarko/software/finder/localization/repository/LocalizationRepository.java
@@ -1,12 +1,9 @@
 package pl.tarko.software.finder.localization.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import pl.tarko.software.finder.localization.model.jpa.Localization;
 
 public interface LocalizationRepository
-        extends JpaRepository<Localization, Long>/*
-                                                     * , QueryDslPredicateExecutor<
-													 * Localization>
-													 */ {
-
+        extends JpaRepository<Localization, Long>, QueryDslPredicateExecutor<Localization> {
 }
