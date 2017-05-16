54f414ddf5d9c406efd29a41f4723bf60cfce5ce
pawel_tarkowski_clurgo <pawel.tarkowski@clurgo.com>
2017-05-16 02:13:01 +0200

    #4 add simples test data

--- localization-finder-core/src/main/java/pl/tarko/software/finder/localization/service/impl/LocalizationServiceImpl.java
+++ localization-finder-core/src/main/java/pl/tarko/software/finder/localization/service/impl/LocalizationServiceImpl.java
@@ -1,6 +1,8 @@
 package pl.tarko.software.finder.localization.service.impl;
 
 import org.springframework.stereotype.Service;
+import pl.tarko.software.finder.localization.model.dto.CityDto;
+import pl.tarko.software.finder.localization.model.dto.CountryDto;
 import pl.tarko.software.finder.localization.model.dto.LocalizationDto;
 import pl.tarko.software.finder.localization.model.dto.LocalizationSearchParamDto;
 import pl.tarko.software.finder.localization.model.jpa.Localization;
@@ -14,6 +16,11 @@ public class LocalizationServiceImpl extends CrudServiceImpl<LocalizationDto, Lo
 
     @Override
     public List<LocalizationDto> findLocalizations(LocalizationSearchParamDto searchParams) {
-        return mapper.mapAsList(repository.findAll(), LocalizationDto.class);
+        List<LocalizationDto> result = mapper.mapAsList(repository.findAll(), LocalizationDto.class);
+        if(result.isEmpty()){
+            LocalizationDto testObject = LocalizationDto.builder().id(1L).address("Test Address").city(CityDto.builder().country(CountryDto.builder().id(1L).name("Polska").build()).build()).latitude(11.22).longitude(33.33).build();
+            result.add(testObject);
+        }
+        return result;
     }
 }
