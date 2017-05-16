ff1d124dc06b1abe68990b9c6e344d0649e95141
pawel_tarkowski_clurgo <pawel.tarkowski@clurgo.com>
2017-05-17 01:03:35 +0200

    #12 add and check cascades

--- localization-finder-core/src/main/java/pl/tarko/software/finder/localization/model/jpa/City.java
+++ localization-finder-core/src/main/java/pl/tarko/software/finder/localization/model/jpa/City.java
@@ -3,10 +3,12 @@ package pl.tarko.software.finder.localization.model.jpa;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @SequenceGenerator(name = "city_sequence", sequenceName = "city_id_seq", allocationSize = 1)
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,11 +19,13 @@ public class City {
     private Long id;
 
     @Column
-    @Setter
     private String name;
 
     @ManyToOne
-    @Setter
+    @JoinColumn(name = "country_id")
     private Country country;
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL)
+    private List<Localization> localizations;
 
 }
