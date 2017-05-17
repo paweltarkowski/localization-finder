7dd1f3e088f651b4601966d919c14f2e30b294a8
pawel_tarkowski_clurgo <pawel.tarkowski@clurgo.com>
2017-05-17 23:20:09 +0200

    #12 remove obsolete code

--- localization-finder-core/src/main/java/pl/tarko/software/finder/localization/service/impl/CityServiceImpl.java
+++ localization-finder-core/src/main/java/pl/tarko/software/finder/localization/service/impl/CityServiceImpl.java
@@ -1,9 +1,8 @@
 package pl.tarko.software.finder.localization.service.impl;
 
-import pl.tarko.software.finder.localization.model.dto.CityDto;
 import pl.tarko.software.finder.localization.model.jpa.City;
 import pl.tarko.software.finder.localization.service.CityService;
 
-public class CityServiceImpl extends CrudServiceImpl<CityDto, City, Long> implements CityService {
+public class CityServiceImpl extends CrudServiceImpl<City, Long> implements CityService {
 
 }
--- localization-finder-core/src/main/java/pl/tarko/software/finder/localization/service/impl/CountryServiceImpl.java
+++ localization-finder-core/src/main/java/pl/tarko/software/finder/localization/service/impl/CountryServiceImpl.java
@@ -1,9 +1,8 @@
 package pl.tarko.software.finder.localization.service.impl;
 
-import pl.tarko.software.finder.localization.model.dto.CountryDto;
 import pl.tarko.software.finder.localization.model.jpa.Country;
 import pl.tarko.software.finder.localization.service.CountryService;
 
-public class CountryServiceImpl extends CrudServiceImpl<CountryDto, Country, Long> implements CountryService {
+public class CountryServiceImpl extends CrudServiceImpl<Country, Long> implements CountryService {
 
 }
